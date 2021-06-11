857cb7683589cd25d13c3c9a4f3089a6550da13b
Raul Oliveira <74179733+sandersonraul@users.noreply.github.com>
2021-06-11 11:30:01 -0300

    Update tarefa-006.rb

--- tarefa-006.rb
+++ tarefa-006.rb
@@ -1,8 +1,8 @@
-h={"a" => 6, "b"=> 3, "c"=> 12}
+h={"a" => 6, "b"=> 3.6, "c"=> 12}
 puts "Valor da chave b: #{h["b"]}"
 h[:e] = 2
 h.each do |key, valor|
-  if valor > 5
+  if valor > 3.5
     puts "Valor maior que 3.5: #{key} = #{valor}"
   end 
 end 
