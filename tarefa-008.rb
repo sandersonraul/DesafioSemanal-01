907190667751e4b5b60480da57e5c19cc5ed3bd0
Raul Oliveira <74179733+sandersonraul@users.noreply.github.com>
2021-06-12 10:22:31 -0300

    Update tarefa-008.rb

--- tarefa-008.rb
+++ tarefa-008.rb
@@ -1,14 +1,15 @@
 a=[]
 i=0
+cont=0
 loop do
   puts "digite um número"
   num = gets.chomp.to_i
   a.push(num)
+  if a[i] >=100 && a[i] <= 200
+      cont = cont + 1	
+  end
   break if num==0
+  i+=1
 end
-while i < a.length do
-    if a[i] >=100 && a[i] <= 200
-      puts a[i]
-    end
-    i+=1
-  end 
+
+puts "Foram digitados #{cont} números entre 100 e 200"
